289787866bb5b9ea31ce7b8af8adadde8dcfa416
amirhosein <amirhosein@cyberdesign.se>
2024-03-31 20:06:43 +0330

    feat: add persistor dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
 
-import store from "./store/store";
+import store, { persistor } from "./store/store";
 
 import SignUp from "./pages/signup/SignUp";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/dashboard/Home";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Provider store={store}>
-          <Routes>
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/dashboard" element={<Home />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
+          <PersistGate persistor={persistor}>
+            <Routes>
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/dashboard" element={<Home />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </PersistGate>
         </Provider>
       </BrowserRouter>
     </>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,38 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/authReducer";
+import storage from "redux-persist/lib/storage";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
-const store = configureStore({
-  reducer: {
-    authReducer,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const rootReducer = combineReducers({
+  auth: authReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
+
+export const persistor = persistStore(store);
