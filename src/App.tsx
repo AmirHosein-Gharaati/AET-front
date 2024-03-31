1b0c24423979b01b970d82ee1566e2589bdd453f
amirhosein <amirhosein@cyberdesign.se>
2024-03-31 17:30:04 +0330

    feat: add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/signup/SignUp";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-      <SignUp />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
