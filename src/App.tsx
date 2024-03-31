621555164ba3ba7ecc91b34e06a24724cbf9317b
amirhosein <amirhosein@cyberdesign.se>
2024-03-31 17:31:18 +0330

    feat: add home page to routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignUp from "./pages/signup/SignUp";
 import NotFound from "./pages/NotFound";
+import Home from "./pages/dashboard/Home";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Home />} />
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
