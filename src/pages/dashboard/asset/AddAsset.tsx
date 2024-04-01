4c293a105deb5ea4f0cce37ad946c04316cc8b08
amirhosein <amirhosein@cyberdesign.se>
2024-04-01 14:38:33 +0330

    feat: add AddAsset page

--- src/pages/dashboard/asset/AddAsset.tsx
+++ src/pages/dashboard/asset/AddAsset.tsx
@@ -1,7 +1,148 @@
+import {
+  Box,
+  Button,
+  Container,
+  OutlinedInput,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
 export default function AddAsset() {
+  const navigate = useNavigate();
+
+  function handleCancel() {
+    navigate("/dashboard/asset");
+  }
+
   return (
-    <>
-      <div>Hello</div>
-    </>
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h1" fontWeight={600} fontSize={28} marginY={2}>
+          Add Asset
+        </Typography>
+
+        <Box>
+          <Box width="100%" marginY="12px">
+            <Typography display="flex">
+              Name{" "}
+              <Typography color="red" marginX="4px">
+                *
+              </Typography>
+            </Typography>
+
+            <OutlinedInput
+              fullWidth
+              sx={{
+                backgroundColor: "white",
+                border: "1px solid #E9E9E9",
+                borderRadius: "10px",
+              }}
+            />
+          </Box>
+
+          <Box width="100%" marginY="12px">
+            <Typography display="flex">
+              Current Price{" "}
+              <Typography color="red" marginX="4px">
+                *
+              </Typography>
+            </Typography>
+
+            <OutlinedInput
+              fullWidth
+              sx={{
+                backgroundColor: "white",
+                border: "1px solid #E9E9E9",
+                borderRadius: "10px",
+              }}
+            />
+          </Box>
+
+          <Box display="flex" marginY="12px">
+            <Box width="100%">
+              <Typography display="flex">
+                Currency Buy{" "}
+                <Typography color="red" marginX="4px">
+                  *
+                </Typography>
+              </Typography>
+
+              <OutlinedInput
+                fullWidth
+                sx={{
+                  backgroundColor: "white",
+                  border: "1px solid #E9E9E9",
+                  borderRadius: "10px",
+                }}
+              />
+            </Box>
+
+            <Box marginX="16px"></Box>
+
+            <Box width="100%">
+              <Typography display="flex">
+                Currency Sell{" "}
+                <Typography color="red" marginX="4px">
+                  *
+                </Typography>
+              </Typography>
+
+              <OutlinedInput
+                fullWidth
+                sx={{
+                  backgroundColor: "white",
+                  border: "1px solid #E9E9E9",
+                  borderRadius: "10px",
+                }}
+              />
+            </Box>
+          </Box>
+        </Box>
+
+        <Box
+          marginY="32px"
+          width="100%"
+          display="flex"
+          justifyContent="space-between"
+        >
+          <Button
+            disableElevation
+            disableRipple
+            onClick={handleCancel}
+            sx={{
+              textTransform: "capitalize",
+              width: "160px",
+              height: "48px",
+              backgroundColor: "#E9E9E9",
+              color: "#51565B",
+              borderRadius: "30px",
+            }}
+          >
+            Cancel
+          </Button>
+
+          <Button
+            variant="contained"
+            disableElevation
+            disableRipple
+            sx={{
+              textTransform: "capitalize",
+              width: "160px",
+              height: "48px",
+              backgroundColor: "#0065F2",
+              borderRadius: "30px",
+            }}
+          >
+            Add
+          </Button>
+        </Box>
+      </Box>
+    </Container>
   );
 }
