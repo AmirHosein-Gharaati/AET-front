0d2461652e72a41e6d5d197e3d362653658dffee
amirhosein <amirhosein@cyberdesign.se>
2024-04-03 19:28:16 +0330

    refactor: revert padding to 2

---
+++ src/components/PageHeader.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+export function PageHeader() {
+  return (
+    <>
+      <Typography component="h1" fontWeight={600} fontSize={28} marginY={2}>
+        Assets
+      </Typography>
+    </>
+  );
+}
