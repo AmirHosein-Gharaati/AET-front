09d9f293e2a87fd41b05ba6c51eb682ad6982f82
amirhosein <amirhosein@cyberdesign.se>
2024-04-03 19:26:48 +0330

    refactor: set padding to 2

--- src/layouts/minimal/MinimalLayout.tsx
+++ src/layouts/minimal/MinimalLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 export default function MinimalLayout() {
   return (
-    <Box sx={{ backgroundColor: "#F2F3F6", height: "100vh" }} paddingX={4}>
+    <Box sx={{ backgroundColor: "#F2F3F6", height: "100vh" }} paddingX={2}>
       <Outlet />
     </Box>
   );
